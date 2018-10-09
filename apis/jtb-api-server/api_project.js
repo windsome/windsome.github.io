d266f395dfccada0128fa1f9db03fbcf2fbe4e29
windsome.feng <36081189@qq.com>
2018-10-09 17:01:33 +0800

    fix 3rdparty auth.

--- apis/jtb-api-server/api_project.js
+++ apis/jtb-api-server/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-10-09T08:34:16.912Z",
+    "time": "2018-10-09T09:01:26.558Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
